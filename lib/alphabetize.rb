3946cbcf7d5e28d83398e78289cf1c6107e9f3da
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-23 15:49:28 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,6 +4,6 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
 
 def alphabetize(arr)
   arr.sort_by do |phrase|
-    phrase.chars.map { |c| ESPERANT0_ALPHABET.index(c) }
+    phrase.chars.map { |c| ESPERANTO_ALPHABET.index(c) }
   end
 end
